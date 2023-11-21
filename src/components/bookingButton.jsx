65055b3302097916d2379e01e0e85d7ff1dc8ddf
justjcurtis <justjcurtis@gmail.com>
2023-11-21 21:25:32 +0000

    feat: create booking button

---
+++ src/components/bookingButton.jsx
@@ -0,0 +1,36 @@
+import colours from '../constants/colours'
+
+const formatPrice = (price) => {
+  return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+const styles = {
+  bookingButton: {
+    fontSize: 12,
+    display: 'flex',
+    flexDirection: 'column',
+    textAlign: 'center',
+    backgroundColor: '#fedd06',
+    borderRadius: 5,
+    color: colours.darkBlue,
+    fontWeight: 'bold',
+    padding: 10,
+    lineHeight: 1.2,
+    cursor: 'pointer',
+    marginTop: 20,
+  },
+  price: {
+    fontSize: 24,
+  }
+}
+
+const BookingButton = ({ price, onClick }) => {
+  return (
+    <div onClick={onClick} style={styles.bookingButton}>
+      <p>Book now</p>
+      <p style={styles.price}>£{formatPrice(price)}</p>
+    </div>
+  );
+}
+
+export default BookingButton;
